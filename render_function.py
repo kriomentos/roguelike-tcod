d4133d64f068a36cd053d09c44e86d7d6703b30f
kriomentos <sznajder.konrad@gmail.com>
2022-06-26 23:16:59 +0200

    Drawing rectangles experiment

--- render_function.py
+++ render_function.py
@@ -11,7 +11,10 @@ if TYPE_CHECKING:
 def render_bar(
     console: Console, current_value: int, maximum_value: int, total_width: int
 ) -> None:
-    bar_width = int(maximum_value - current_value * total_width) # int(float(current_value) / maximum_value * total_width)
+    # it ends up as 0 for current_value < 3, which makes it be empty
+    # while player is still alive :)
+    # i can't into basic maths
+    bar_width = int(float(current_value) / maximum_value * total_width)
 
     console.draw_rect(
         x = 84, 
@@ -22,17 +25,17 @@ def render_bar(
         bg = color.bar_filled
     )
 
-    if bar_width > 0:
+    if bar_width >= 0:
         console.draw_rect(
             x = 84,
             y = 2,
             width = 1,
-            height = bar_width,
+            height = total_width - bar_width,
             ch = 1,
             bg = color.bar_empty
         )
 
     console.print(
         x = 80, y = 0,
-        string = f"HP: {current_value}/{maximum_value}", fg = color.bar_text
+        string = f"HP:\n {current_value}/\n{maximum_value}", fg = color.bar_text
     )
