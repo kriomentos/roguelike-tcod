edf9372a4d95548a854b6efa75b5ee0d38dd5b29
kriomentos <sznajder.konrad@gmail.com>
2023-08-21 16:56:31 +0200

    Changes to rng Generator

--- helpers/diggers.py
+++ helpers/diggers.py
@@ -1,15 +1,17 @@
+from __future__ import annotations
+
 from tcod import los
 from game_map import GameMap
 
 from typing import Tuple, Iterator
-from helpers.rng import nprng
+from numpy.random import Generator
 
-def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
+def tunnel_between(start: Tuple[int, int], end: Tuple[int, int], rand_generator: Generator) -> Iterator[Tuple[int, int]]:
     # return L shaped tunnel between two points
     x1, y1 = start
     target_x, target_y = end
 
-    if nprng.random() < 0.5:
+    if rand_generator.random() < 0.5:
         # go horizontal, then vertical
         corner_x, corner_y = target_x, y1
     else:
@@ -21,19 +23,19 @@ def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tup
     for x, y in los.bresenham((corner_x, corner_y), (target_x, target_y))[1:-1].tolist():
         yield x, y
 
-def drunken_walk(start: Tuple[int, int], end: Tuple[int, int], dungeon: GameMap) -> Iterator[Tuple[int, int]]:
+def drunken_walk(start: Tuple[int, int], end: Tuple[int, int], dungeon: GameMap, rand_generator: Generator) -> Iterator[Tuple[int, int]]:
     x, y = start
     target_x, target_y = end
 
     while (x, y) != end:
         if x < target_x:
-            x += nprng.choice([0, 1])
+            x += rand_generator.choice([0, 1])
         elif x > target_x:
-            x += nprng.choice([0, 1])
+            x += rand_generator.choice([0, 1])
         if y < target_y:
-            y += nprng.choice([0, 1])
+            y += rand_generator.choice([0, 1])
         elif y > target_y:
-            y += nprng.choice([0, 1])
+            y += rand_generator.choice([0, 1])
 
         x = max(1, min(x, dungeon.width - 1))
         y = max(1, min(y, dungeon.height - 1))
