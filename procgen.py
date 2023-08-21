d81a6b224b55aadc62805f5761dc35e88edd8fa7
kriomentos <sznajder.konrad@gmail.com>
2023-08-21 17:23:13 +0200

    Hack to skip downstairs

--- procgen.py
+++ procgen.py
@@ -174,9 +174,9 @@ def generate_dungeon(
     for _ in range(convolve_steps):
         cellular_automata(dungeon, 4, wall_count)
 
-    x, y = np.where(dungeon.tiles["walkable"])
-    j = nprng.integers(len(x))
-    player.place(x[j], y[j], dungeon)
+    # x, y = np.where(dungeon.tiles["walkable"])
+    # j = nprng.integers(len(x))
+    # player.place(x[j], y[j], dungeon)
 
     for _ in range(1):
         generate_rooms(dungeon, 10, 4, 10, nprng)
